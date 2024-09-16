4b54129f9441fbff3581daebfb87b798fc630269
Samantha Morales <samanthamp43@gmail.com>
2024-09-16 16:44:58 -0500

    Added Samantha Morales

--- Hello.java
+++ Hello.java
@@ -2,9 +2,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Hello Cris Perales");
-<<<<<<< HEAD
     System.out.println("Hello Jonathan");
-=======
     System.out.println("Hello Mason Toy");
     System.out.println("Hello Michael Hannan");
     System.out.println("Hello Jeff Maxwell");
@@ -16,6 +14,5 @@ public class Hello {
     System.out.println("Hello Noah McMullan");
     System.out.println("Helloo Jonathan");
     System.out.println("Hello Samantha Morales");
->>>>>>> 43fd0f233c164ffe2a01a7f161cd9b1115c8aef7
   }
 }
